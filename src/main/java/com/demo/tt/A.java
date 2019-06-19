82e09e0f12de84dd6b04ee55972d5ee87f4f5ef1
lwg142857 <2371615445@qq.com>
2019-06-19 15:45:39 +0800

    add A.java

---
+++ src/main/java/com/demo/tt/A.java
@@ -0,0 +1,11 @@
+package com.demo.tt;
+
+/**
+ * @Authot : lwg
+ * @Date : Created in 15:44 2019/6/19
+ */
+public class A {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
